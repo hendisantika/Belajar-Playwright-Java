bafe68ade53eeac921784fc77d3565e90d802ed3
hendisantika <hendisantika@yahoo.co.id>
2023-12-04 11:45:29 +0700

    Handle Modal Test Case 🫸🌀✏️📗🐧🐳️

--- src/test/java/id/co/pkp/App1Test.java
+++ src/test/java/id/co/pkp/App1Test.java
@@ -562,6 +562,12 @@ public class App1Test {
         });
         popUp.waitForLoadState();
         System.out.println(popUp.title());
+
+        page.navigate("http://autopract.com/selenium/popup/");
+        page.locator("//a[normalize-space()='JQuery Popup Model']").click();
+        String textContent = page.locator("//p[normalize-space()='This is Sample Popup.']").textContent();
+        System.out.println(textContent);
+
         page.close();
         browser.close();
         playwright.close();
