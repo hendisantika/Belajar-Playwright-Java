2bf6e14dce30cf84a182b6bd6cca949a478eba27
hendisantika <hendisantika@yahoo.co.id>
2023-12-04 18:32:13 +0700

    Check Frame Test Cases 🫸🌀✏️📗🐧🐳️

--- src/test/java/id/co/pkp/App1Test.java
+++ src/test/java/id/co/pkp/App1Test.java
@@ -3,6 +3,7 @@ package id.co.pkp;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.microsoft.playwright.Dialog;
+import com.microsoft.playwright.Frame;
 import com.microsoft.playwright.*;
 import com.microsoft.playwright.options.RequestOptions;
 import org.junit.jupiter.api.Disabled;
@@ -21,6 +22,7 @@ import java.util.Base64;
 import java.util.HashMap;
 import java.util.List;
 
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
@@ -861,6 +863,106 @@ public class App1Test {
         page.close();
         browser.close();
         playwright.close();
+    }
+
+    @Test
+    @DisplayName("Check Frame Test")
+    public void checkFrameTest() throws InterruptedException {
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        //  This line creates a new page object within the browser instance. The page object represents a single web page that can be navigated, interacted with, and tested.(It's kind of tab but exactly a tab.)
+        Page page = browser.newPage();
+        //  This code instructs the page object to navigate to the specified URL
+        page.navigate("https://the-internet.herokuapp.com/");
+        System.out.println(page.title());
+        assertThat(page).hasTitle("The Internet");
+        // Wait for the element to be available Wait are optional.
+        page.waitForSelector("//a[contains(@href, '/frames')]");
+        // Click the element
+        page.click("//a[contains(@href, '/frames')]");
+        // How to handle a single frame and interact with an element within it.
+        // Go to Iframe example
+        page.click("a[href='/iframe']");
+        // get all the frames in the page to a list.
+        List<Frame> frames = page.frames();
+        System.out.println("No.of frames available: " + frames.size());
+
+        // In Playwright we don't have to switch to the frame. we just have to locate the frame.
+        FrameLocator myFrame = page.frameLocator("//iframe[@id='mce_0_ifr']");
+        // Then related to the frame write the locator of element that I want to interact.
+        Locator textFieldBody = myFrame.locator("//body[@id='tinymce']");
+        textFieldBody.click();
+        textFieldBody.clear();
+        textFieldBody.type("Typed inside of the text field of Imframe");
+
+        // Or you can  define the Locator inside of the frame using frame in one go with just one Locator variable.
+        Locator textFieldBodyDirect = page.frameLocator("//iframe[@id='mce_0_ifr']").locator("//body[@id='tinymce']");
+        textFieldBodyDirect.click();
+        textFieldBodyDirect.clear();
+        textFieldBodyDirect.type("Handle the element in the frame in one go");
+
+        // How to handle Nested frames, frame within a frame.
+        page.navigate("https://letcode.in/frame");
+
+        // There first name and last name are inside of just 1 frame.
+        // Let's get the First frame to a locator,
+        FrameLocator firstFrame = page.frameLocator("//iframe[@id='firstFr']");
+        // Below first name element is inside the frame
+        Locator firstName = firstFrame.locator("//input[@name='fname']");
+        firstName.type("Hello First name");
+        Locator lastName = firstFrame.locator("//input[@name='lname']");
+        lastName.type("Hello Last name");
+
+        // Email field is inside of the above firstFrame
+        FrameLocator nestedSecondFrame = firstFrame.frameLocator("//iframe[@class='has-background-white']");
+        // Now let's enter the values at Enter email is in Email fild.
+        Locator emailInsideOfTwoFrames = nestedSecondFrame.locator("//input[@name='email']");
+        emailInsideOfTwoFrames.type("johndoe@yahoo.co.uk");
+
+        // optional codes to get an idea about frame content of the page.
+        List<Frame> multipleFrames = page.frames();
+        System.out.println("No.of frames available: " + multipleFrames.size());
+        // Print all the URLS of frame
+        multipleFrames.forEach(frame -> System.out.println(frame.url()));
+       /* every iframe element has a unique URL. The URL of an iframe element is the URL of the page that is embedded in the iframe.
+        Example <iframe src="https://www.google.com"></iframe>  in Facebook page we can add a link to google.
+         url means src value of iframe
+        The src value is the attribute that specifies the URL
+        <iframe _ngcontent-serverapp-c66="" src="frameUI" id="firstFr" name="firstFr"></iframe>
+        */
+
+        Thread.sleep(24000);
+        //  Frame frame is old way don't use it   Frame frame = page.frame("//iframe[@id='mce_0_ifr']");
+        page.close();
+        // Closes the browser window(s) associated with the browser instance.
+        browser.close();
+        // playwright.close() command is used to close the page that is currently open in the Chromium browse
+        playwright.close();
+    }
+
+    @Test
+    @DisplayName("Check Frame Test 2")
+    public void checkFrameTest2() {
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch();
+
+        // creating a BrowserContext
+        BrowserContext browserContext = browser.newContext();
+
+        Page page = browserContext.newPage();
+
+        // Navigating to the URL
+        page.navigate("https://testkru.com/Interactions/Frames");
+
+        Frame frame = page.frame("singleFrame");
+
+        Locator locator = frame.locator("h1");
+        System.out.println("Text written inside the frame: " + locator.innerText());
+
+        // closing the instances
+        browser.close();
+        playwright.close();
+
     }
 
 }
