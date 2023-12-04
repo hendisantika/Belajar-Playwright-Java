f627ba129af063371be0d069a7c8a2c4cce286c7
hendisantika <hendisantika@yahoo.co.id>
2023-12-04 11:44:32 +0700

    Handle Popup Test Case 🫸🌀✏️📗🐧🐳️

--- src/test/java/id/co/pkp/App1Test.java
+++ src/test/java/id/co/pkp/App1Test.java
@@ -548,4 +548,23 @@ public class App1Test {
         playwright.close();
     }
 
+    @Test
+    @DisplayName("Handle Popup in Playwright Java")
+    public void handlePopup() {
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        Page page = browser.newPage();
+
+        page.navigate("http://autopract.com/selenium/popup/");
+
+        Page popUp = page.waitForPopup(() -> {
+            page.locator("//a[normalize-space()='Open Link in Popup']").click();
+        });
+        popUp.waitForLoadState();
+        System.out.println(popUp.title());
+        page.close();
+        browser.close();
+        playwright.close();
+    }
+
 }
